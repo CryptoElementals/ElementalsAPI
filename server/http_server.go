b77811e843e1b64975c2edeef9b66341956bece9
tangjie <jietang.witcet@gmail.com>
2025-08-04 14:07:16 +0800

    add test frontend website

--- server/http_server.go
+++ server/http_server.go
@@ -147,6 +147,8 @@ func corsMiddleware() gin.HandlerFunc {
 			"http://127.0.0.1:3000",
 			"http://localhost:8080",
 			"http://127.0.0.1:8080",
+			"http://beast-royale-fe.prj-console.svc.a4.u4/",
+			"https://beast-royale-fe.prj-console.svc.a4.u4/",
 		}
 
 		// 检查来源是否被允许
