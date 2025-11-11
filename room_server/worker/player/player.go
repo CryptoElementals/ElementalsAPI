62613fa9010c3f6032b8a8a8b0f4e3301aef1447
athos <athos@dill.xyz>
2025-11-11 19:59:13 +0800

    feat: publish data with event

--- room_server/worker/player/player.go
+++ room_server/worker/player/player.go
@@ -2,11 +2,14 @@ package player
 
 import (
 	"context"
+	"errors"
 
+	"github.com/CryptoElementals/common/conversion"
 	"github.com/CryptoElementals/common/log"
 	"github.com/CryptoElementals/common/room_server/worker"
 	"github.com/CryptoElementals/common/room_server/worker/types"
 	"github.com/CryptoElementals/common/rpc/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type Player struct {
@@ -63,10 +66,20 @@ func (p *Player) handleNewGameEvent(ctx context.Context, evt *types.GameCreatedE
 	// send nothing if continued game
 	if !evt.IsContinueGame {
 		log.Debugw("publish event", "event type", proto.EventType_TYPE_MATCHED, "receiver", p.address.String(), "game id", evt.GameID)
+		players := make([]*proto.PlayerAddress, 0, len(evt.Players))
+		for _, player := range evt.Players {
+			players = append(players, player.ToProto())
+		}
 		p.publisher.Publish(ctx, &proto.PublishRequest{
 			Topic: p.address.String(),
 			Event: &proto.Event{
 				Type: proto.EventType_TYPE_MATCHED,
+				Event: &proto.Event_GameCreated{
+					GameCreated: &proto.GameCreated{
+						GameId:  uint32(evt.GameID),
+						Players: players,
+					},
+				},
 			},
 		})
 	}
@@ -79,6 +92,12 @@ func (p *Player) handleGameReadyEvent(ctx context.Context, evt *types.GameReadyE
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_GAME_CREATED,
+			Event: &proto.Event_GameReady{
+				GameReady: &proto.GameReady{
+					GameId:          uint32(evt.GameID),
+					ContractAddress: evt.ContractAddress,
+				},
+			},
 		},
 	})
 	return nil
@@ -94,6 +113,9 @@ func (p *Player) handleRoundPartialReadyEvent(ctx context.Context, evt *types.Ro
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_PART_CONFIRMED,
+			Event: &proto.Event_X{
+				X: &emptypb.Empty{},
+			},
 		},
 	})
 	return nil
@@ -105,6 +127,12 @@ func (p *Player) handleRoundReadyEvent(ctx context.Context, evt *types.RoundRead
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_ROUND_READY,
+			Event: &proto.Event_RoundReady{
+				RoundReady: &proto.RoundReady{
+					GameId:   uint32(evt.GameID),
+					RoundNum: evt.RoundNumber,
+				},
+			},
 		},
 	})
 	return nil
@@ -116,6 +144,12 @@ func (p *Player) handleCommitmentsOnChainEvent(ctx context.Context, evt *types.C
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_COMMITMENTS_ON_CHAIN,
+			Event: &proto.Event_CommitmentsOnChain{
+				CommitmentsOnChain: &proto.CommitmentsOnChain{
+					GameId:   uint32(evt.GameID),
+					RoundNum: evt.RoundNumber,
+				},
+			},
 		},
 	})
 	return nil
@@ -127,6 +161,12 @@ func (p *Player) handleCardsOnChainEvent(ctx context.Context, evt *types.CardsOn
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_CARDS_ON_CHAIN,
+			Event: &proto.Event_CardsOnChain{
+				CardsOnChain: &proto.CardsOnChain{
+					GameId:   uint32(evt.GameID),
+					RoundNum: evt.RoundNumber,
+				},
+			},
 		},
 	})
 	return nil
@@ -138,6 +178,9 @@ func (p *Player) handleContinueCanceledEvent(ctx context.Context, evt *types.Con
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_CONTINUE_CANCELED,
+			Event: &proto.Event_X{
+				X: &emptypb.Empty{},
+			},
 		},
 	})
 	return nil
@@ -145,28 +188,74 @@ func (p *Player) handleContinueCanceledEvent(ctx context.Context, evt *types.Con
 
 func (p *Player) handleRoundCompletedEvent(ctx context.Context, evt *types.RoundCompletedEvent) error {
 	log.Debugw("publish event", "event type", proto.EventType_TYPE_ROUND_COMPLETE, "receiver", p.address.String(), "game id", evt.GameID)
+
+	// Convert dao.Round to proto.Round (rpc.Round)
+	var roundInfo *proto.Round
+	if evt.RoundInfo != nil {
+		roundInfo = conversion.DbGameRoundToProtoGameRound(evt.RoundInfo)
+	}
+
 	p.publisher.Publish(ctx, &proto.PublishRequest{
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_ROUND_COMPLETE,
+			Event: &proto.Event_RoundCompleted{
+				RoundCompleted: &proto.RoundCompleted{
+					GameId:    uint32(evt.GameID),
+					RoundInfo: roundInfo,
+				},
+			},
 		},
 	})
 	return nil
 }
 
 func (p *Player) handleGameCompletedEvent(ctx context.Context, evt *types.GameCompletedEvent) error {
+	if evt.GameInfo == nil {
+		return errors.New("game info is nil")
+	}
+	// Get the last round from GameInfo if available
+	var roundInfo *proto.Round
+	if len(evt.GameInfo.Rounds) > 0 {
+		// Get the last round
+		lastRound := evt.GameInfo.Rounds[len(evt.GameInfo.Rounds)-1]
+		roundInfo = conversion.DbGameRoundToProtoGameRound(lastRound)
+	}
+
 	log.Debugw("publish event", "event type", proto.EventType_TYPE_ROUND_COMPLETE, "receiver", p.address.String(), "game id", evt.GameID)
 	p.publisher.Publish(ctx, &proto.PublishRequest{
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_ROUND_COMPLETE,
+			Event: &proto.Event_RoundCompleted{
+				RoundCompleted: &proto.RoundCompleted{
+					GameId:    uint32(evt.GameID),
+					RoundInfo: roundInfo,
+				},
+			},
 		},
 	})
+
+	// Convert dao.GameResult to battle.GameResult
+	var battleGameResult *proto.GameResult
+	if evt.GameInfo.GameResult != nil {
+		if evt.GameInfo.GameResult != nil {
+			battleGameResult = conversion.DbGameResultToProtoGameResult(evt.GameInfo.GameResult)
+			battleGameResult.GameContinueTimeout = uint64(evt.GameInfo.GameArgs.ContinueTimeout)
+		}
+	}
+
 	log.Debugw("publish event", "event type", proto.EventType_TYPE_GAME_COMPLETE, "receiver", p.address.String(), "game id", evt.GameID)
 	p.publisher.Publish(ctx, &proto.PublishRequest{
 		Topic: p.address.String(),
 		Event: &proto.Event{
 			Type: proto.EventType_TYPE_GAME_COMPLETE,
+			Event: &proto.Event_GameCompleted{
+				GameCompleted: &proto.GameCompleted{
+					GameId:     uint32(evt.GameID),
+					GameResult: battleGameResult,
+				},
+			},
 		},
 	})
 	return nil
