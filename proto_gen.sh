366d4cc664258c7d9c48a1fe8cbc04af1a09641c
tangjie <jietang.witcet@gmail.com>
2026-01-26 17:12:29 +0800

    proto_gen.sh

--- proto_gen.sh
+++ proto_gen.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-export PATH=$PATH:~/go/bin
-# protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative rpc/proto/*.proto
-protoc --experimental_allow_proto3_optional --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative rpc/proto/*.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative rpc/proto/*.proto
