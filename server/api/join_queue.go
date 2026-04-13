45484553ca05fa4e3d004fceac8e48c27a2c436d
patrick.sky <patricksky131@gmail.com>
2026-04-13 16:11:23 +0800

    add errcode for API JoinQueue

--- server/api/join_queue.go
+++ server/api/join_queue.go
@@ -144,9 +144,25 @@ func (task *JoinQueueTask) Run(c *gin.Context) (Response, error) {
 
 	_, err = lobbyClient.JoinQueue(context.Background(), playerAddr)
 	if err != nil {
-		task.Response.BaseResponse.RetCode = 1002
-		task.Response.BaseResponse.Message = "Lobby JoinQueue failed: " + err.Error()
-		return task.Response, nil
+		shortErr := ShortGRPCError(err)
+		// 检查apiserver的TokenThreshold配置是否与roomserver一致
+		if strings.Contains(shortErr, "user token is not enough") {
+			task.Response.BaseResponse.RetCode = 1004
+			//task.Response.BaseResponse.Message = fmt.Sprintf("Insufficient available tokens, need at least %d tokens to join match queue", config.GameParams.TokenThreshold)
+			task.Response.BaseResponse.Message = "Insufficient available tokens"
+		} else if strings.Contains(shortErr, "player cannot join queue, player status: PLAYER_IN_GAME") {
+			// 玩家已经在对局中，不能再次加入匹配队列，返回业务错误码 1006
+			task.Response.BaseResponse.RetCode = 1006
+			task.Response.BaseResponse.Message = "Player is already in game and cannot join match queue"
+		} else if strings.Contains(shortErr, "player cannot join queue, player status: PLAYER_MATCHED") {
+			// 玩家已经匹配上在等待确认，不能再次加入匹配队列，返回业务错误码 1007
+			task.Response.BaseResponse.RetCode = 1007
+			task.Response.BaseResponse.Message = "Player is already matched and cannot join match queue"
+		} else {
+			task.Response.BaseResponse.RetCode = 1002
+			task.Response.BaseResponse.Message = "Lobby JoinQueue failed: " + err.Error()
+			return task.Response, nil
+		}
 	}
 
 	// roomserver 进行匹配
